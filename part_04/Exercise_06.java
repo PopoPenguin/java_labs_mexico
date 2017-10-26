836e3a715e419a97f47eb5194fff0f18b2327589
Jialor Cheung <jialorcheung@gmail.com>
2017-10-26 11:28:50 -0500

    updated part 04 Exercise 6

--- part_04/Exercise_06.java
+++ part_04/Exercise_06.java
@@ -2,9 +2,9 @@ package part_04;
 
 /**
 
- Implement, and populate a non-circular, consumptive queue (once a value has been retrieved
+ Implement, and populate a non-circular, consumptive generic_queue (once a value has been retrieved
  it cannot be retrieved again) of integers 0 through 100. Then print out every other
- value in the queue.
+ value in the generic_queue.
 
  expected output: 0,2,4,6,8.....100
 
@@ -13,10 +13,10 @@ class Exercise_06 {
 
     public static void main(String[] args) {
 
-        Queue test = new Queue(101);        //creating a new queue of size 101
+        Queue test = new Queue(101);        //creating a new generic_queue of size 101
 
-        for (int i = 0; i<test.size(); i++){           //populating queue incrementally from 0 to 100
-            test.put(i);                       //placing into the queue
+        for (int i = 0; i<test.size(); i++){           //populating generic_queue incrementally from 0 to 100
+            test.put(i);                       //placing into the generic_queue
         }
 
 
@@ -32,22 +32,22 @@ class Exercise_06 {
 }
 
 
-//creating int queue class of parameter entered
+//creating int generic_queue class of parameter entered
 
 class Queue {
 
     private int [] q;                           //declares an instance variable q that is an array
     private int putLocation, getLocation;       //declares two instance variable for the put and get location
 
-    Queue (int a){                              //creates a class for queue
+    Queue (int a){                              //creates a class for generic_queue
         q = new int[a];
         putLocation = getLocation = 0;          //initializes the value of put and get to 0
 
     }
 
     public void put(int num) {                  //creates a put method
-        if (putLocation == q.length) {                  //tests if the queue is full
-            System.out.println("Queue is full");
+        if (putLocation == q.length) {                  //tests if the generic_queue is full
+            System.out.println("GenericQueue is full");
             return;
         }
         q[putLocation++] = num;                 //if not takes integer and places in q[putLocation] then increments by 1
@@ -57,7 +57,7 @@ class Queue {
 
     public int get() {                          //create a get method
         if (getLocation==putLocation){          //if getLocation is not the same as putLocation
-            System.out.println("Spot is empty");    //states queue is empty and returns 0
+            System.out.println("Spot is empty");    //states generic_queue is empty and returns 0
             return 0;
         }
 
